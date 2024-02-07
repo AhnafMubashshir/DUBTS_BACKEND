73581d829ca4b093ee52495baac693c5bdf029cf
Md_Siam <siam@xpeedlab.tech>
2024-02-07 17:42:08 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// connectDB();
+connectDB();
 
 const locationRoutes = require("./routes/locationRoutes");
 app.use("/dubts/bus-location", locationRoutes);
